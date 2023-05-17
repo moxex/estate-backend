0fb1bc5c14762a46899a40980948ce735de3589f
moxexgit config --global user.email akojimoses@gmail.com <akojimoses@gmail.com>
2023-05-17 13:10:24 +0100

    Updated Urls for props

--- apps/properties/urls.py
+++ apps/properties/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('all/', views.ListAgentsPropertiesAPIView.as_view(), name='all-properties'),
+    path('all/', views.ListAllPropertiesAPIView.as_view(), name='all-properties'),
     path('agents/', views.ListAgentsPropertiesAPIView.as_view(), name='agent-properties'),
     path('create/', views.create_property_api_view, name='property-create'),
     path('details/<slug:slug>/', views.PropertyDetailView.as_view(), name='property-details'),
